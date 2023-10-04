32c09b687168652a34f5e8cfe28c94eaf9f0bc49
Wahiba BOUZYAN <ouahibabouzyan@gmail.com>
2023-10-04 15:52:49 +0100

    Initial commit

---
+++ src/main/java/org/sid/demospringcloudstreamkafka/DemoSpringCloudStreamKafkaApplication.java
@@ -0,0 +1,12 @@
+package org.sid.demospringcloudstreamkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringCloudStreamKafkaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringCloudStreamKafkaApplication.class, args);
+    }
+
+}
